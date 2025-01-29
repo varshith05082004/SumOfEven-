f45bd4748958a180e8e42cefe3784fdbb2c370bf
varshith05082004 <varshithreddybobbali@gmail.com>
2025-01-29 22:23:13 +0530

    Add files via upload

---
+++ SumOfEven.java
@@ -0,0 +1,12 @@
+public class SumOfEven {
+    public static void main(String[] args) {
+        int sum=0;
+        for(int i=0;i<=100;i++){
+            if(i%2==0){
+                sum=sum+i;
+            }
+        }System.out.println(sum);
+    }
+}
+
+
